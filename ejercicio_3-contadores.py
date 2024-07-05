cc05b6e802104d1690ab5cffe925e5419c051d8a
lmirabelli <chuchenko1986@gmail.com>
2024-07-05 09:24:47 -0300

    Ej3

---
+++ ejercicio_3-contadores.py
@@ -0,0 +1,11 @@
+number = int(input("ingresa un número: "))
+
+print("el numero elegido fue: " + str(number))
+
+number10 = number+10
+
+for i in range(10):
+    number += 1
+    print(number)
+
+print('...')
